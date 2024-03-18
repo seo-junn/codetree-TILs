c2cc7f10fff02d8323a2df880758d3ff40ad80b7
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-18 22:36:45 +0900

    Title: 가장 짧은 부분합, Time: 171ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240318/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,19 @@
+import sys
+
+n, s = map(int,sys.stdin.readline().split())
+nums = list(map(int,sys.stdin.readline().split()))
+
+length = n+1
+start = 0
+end = 0
+total = 0
+
+while end < n:
+    total += nums[end]
+    while total >= s:
+        length = min(length,end-start+1)
+        total -= nums[start]
+        start += 1
+    end += 1
+
+print(length if length < n+1 else -1)
