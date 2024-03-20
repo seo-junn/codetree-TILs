fe79fea4c2de6b85aab861de93faf067aa4b83da
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-20 21:30:03 +0900

    Title: 0에 가장 가까운 합, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240320/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(sys.stdin.readline())
+nums = list(sorted(map(int,sys.stdin.readline().split())))
+
+left = 0
+right = n-1
+ans = 10**9
+
+while left < right:
+    now = nums[left] + nums[right]
+    if now > 0:
+        right -= 1
+    elif now < 0:
+        left += 1
+    else:
+        print(0)
+        exit(0)
+    ans = min(ans,abs(now))
+
+print(ans)
