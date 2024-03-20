2904d32d6a699f5bb06134e93df4a84bf8bdb7dc
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-20 22:04:55 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 1684ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240320/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 240320/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,6 +1,39 @@
 import sys
+import bisect
 
 N, D = map(int,sys.stdin.readline().split())
+dots = {}
+for _ in range(N):
+    x, y = map(int,sys.stdin.readline().split())
+    try:
+        dots[y].append(x)
+    except:
+        dots[y] = [x]
+
+index = list(sorted(dots.keys()))
+if index[-1]-index[0] < D:
+    print(-1)
+    exit(0)
+
+dist = 10**7
+for left in range(len(index)):
+    right = len(index)-1
+    while left < right:
+        if index[right] - index[left] >= D:
+            for lx in dots[index[left]]:
+                lidx = bisect.bisect_left(dots[index[right]],lx)
+                ridx = bisect.bisect_right(dots[index[right]],lx)
+                if lidx == ridx:
+                    dist = min(dist,abs(dots[index[right]][0]-lx),abs(dots[index[right]][-1]-lx))
+                else:
+                    dl = abs(dots[index[right]][bisect.bisect_left(dots[index[right]],lx)]-lx)
+                    dr = abs(dots[index[right]][bisect.bisect_right(dots[index[right]],lx)-1]-lx)
+                
+        right -= 1
+
+print(dist)
+
+'''
 dots = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
 dots.sort(key=lambda x: (x[1],x[0]))
 
@@ -20,3 +53,4 @@ for left in range(N):
             break
 
 print(dist)
+'''
