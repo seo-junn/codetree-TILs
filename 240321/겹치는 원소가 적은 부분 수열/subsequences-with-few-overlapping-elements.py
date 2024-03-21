8dd6a102533d928ab4f34b822921ad597bb7b60b
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-21 14:23:03 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 148ms, Memory: 35MB, Status: Passed - CodeTree

--- 240321/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.py
+++ 240321/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.py
@@ -3,21 +3,25 @@ import sys
 n, k = map(int,sys.stdin.readline().split())
 nums = list(map(int,sys.stdin.readline().split()))
 
-cache = [0]*(max(nums)+1)
+cache = {}
 
 start = 0
 end = 0
 length = 0
 
 while end < n:
-    if cache[nums[end]] < k:
-        cache[nums[end]] += 1
+    try:
+        if cache[nums[end]] < k:
+            cache[nums[end]] += 1
+            end += 1
+        else:
+            length = max(length,end-start)
+            while cache[nums[end]] >= k:
+                cache[nums[start]] -= 1
+                start += 1
+    except:
+        cache[nums[end]] = 1
         end += 1
-    else:
-        length = max(length,end-start)
-        while cache[nums[end]] >= k:
-            cache[nums[start]] -= 1
-            start += 1
 
 length = max(length,end-start)
 print(length)
