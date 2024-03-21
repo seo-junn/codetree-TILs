324585cf7af00c1c7ccf560c1f61db641af15138
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-21 12:40:29 +0900

    Title: 특정 조건으로 묶여있는 숫자 그룹, Time: 222ms, Memory: 29MB, Status: Passed - CodeTree

--- 240321/특정 조건으로 묶여있는 숫자 그룹/a-group-of-numbers-tied-to-specific-conditions.py
+++ 240321/특정 조건으로 묶여있는 숫자 그룹/a-group-of-numbers-tied-to-specific-conditions.py
@@ -3,25 +3,30 @@ import sys
 N, K = map(int,sys.stdin.readline().split())
 nums = list(sorted(map(int,sys.stdin.read().splitlines())))
 
-start = 0
-end = 0
-cand = []
+L, R = [0]*N, [0]*N
 
-while end < N:
-    if nums[end] - nums[start] <= K:
-        end += 1
-    else:
-        cand.append((end-start,start,end-1))
-        while nums[end] - nums[start] > K:
-            start += 1
+count = 0
+i = 0
+for j in range(N):
+    while i < N and nums[j]-nums[i] > K:
+        i += 1
+    
+    count = max(count, j - i + 1)
 
-cand.append((end-start,start,end-1))
-cand.sort()
+    L[j] = count
 
-ans = cand[-1][0]
-for i in range(len(cand)-2,-1,-1):
-    if cand[i][-1] < cand[-1][1] or cand[i][1] > cand[-1][-1]:
-        ans += cand[i][0]
-        break
+count = 0
+j = N-1
+for i in range(N-1,-1,-1):
+    while j - 1 >= i and nums[j]-nums[i] > K:
+        j -= 1
+    
+    count = max(count, j - i + 1)
+
+    R[i] = count
+
+ans = L[N-1]
+for i in range(N-1):
+    ans = max(ans,L[i]+R[i+1])
 
 print(ans)
