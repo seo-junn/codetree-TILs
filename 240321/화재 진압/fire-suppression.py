bcd8e727b602e090fe6e44beba94c56d77d71804
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-21 16:59:57 +0900

    Title: 화재 진압, Time: 173ms, Memory: 34MB, Status: Passed - CodeTree

--- 240321/화재 진압/fire-suppression.py
+++ 240321/화재 진압/fire-suppression.py
@@ -4,17 +4,14 @@ n, m = map(int,sys.stdin.readline().split())
 fire = list(sorted(map(int,sys.stdin.readline().split())))
 fighter = list(sorted(map(int,sys.stdin.readline().split())))
 
+j = 0
 length = 0
-ff_idx = 0
-
 for i in range(len(fire)):
-    dist = abs(fighter[ff_idx] - fire[i])
-    if ff_idx < len(fighter)-1:
-        dist2 = abs(fighter[ff_idx+1]-fire[i])
-        if dist2 < dist:
-            dist = dist2
-            ff_idx += 1
-
+    dist = 2*10**9
+    while j < len(fighter) and dist > abs(fire[i]-fighter[j]):
+        dist = abs(fire[i]-fighter[j])
+        j += 1
+    j -= 1
     length = max(length,dist)
 
 print(length)
