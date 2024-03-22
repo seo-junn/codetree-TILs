e846c482bf22ae64095c03e1c634021e03a92d4d
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 20:24:25 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 5052ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240322/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240322/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -2,22 +2,34 @@ import sys
 from functools import cmp_to_key
 
 def compare(x, y):
-    if x + y < y + x:
+    if x[0]*y[1] > x[1]*y[0]:
         return -1
-    if x + y > y + x:
+    if x[0]*y[1] < x[1]*y[0]:
         return 1
     return 0
 
-N = int(sys.stdin.readline())
-chunks = sys.stdin.read().splitlines()
-chunks.sort(key=cmp_to_key(compare))
-res = ''.join(chunks)
+n = int(sys.stdin.readline())
 
+chunks = []
+for _ in range(n):
+    line = sys.stdin.readline().strip()
+    lc,rc = 0,0
+    for c in line:
+        if c == '(': lc += 1
+        else: rc += 1
+    count = 0
+    for i in range(len(line)):
+        if line[i] == '(':
+            for j in range(i+1,len(line)):
+                if line[j] == ')':
+                    count += 1
+    chunks.append((lc,rc,count))
+
+chunks.sort(key=cmp_to_key(compare))
 score = 0
-for i in range(len(res)):
-    if res[i] == '(':
-        for j in range(i+1,len(res)):
-            if res[j] == ')':
-                score += 1
+for i in range(n):
+    score += chunks[i][-1]
+    for j in range(i+1,n):
+        score += chunks[i][0]*chunks[j][1]
 
 print(score)
