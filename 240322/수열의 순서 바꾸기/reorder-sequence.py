4ad0afa322708e44f6eed0413335bc3f5401e803
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 22:59:05 +0900

    Title: 수열의 순서 바꾸기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/수열의 순서 바꾸기/reorder-sequence.py
+++ 240322/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,7 +1,9 @@
 N = int(input())
 nums = list(map(int,input().split()))
 
+ans = 0
 for i in range(N-2,-1,-1):
     if nums[i] > nums[i+1]:
-        print(i+1)
+        ans = i+1
         break
+print(ans)
