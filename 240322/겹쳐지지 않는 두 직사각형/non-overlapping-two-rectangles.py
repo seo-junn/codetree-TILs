b64f954f86451395d557b9cadc673d2f2543b9a5
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 16:39:35 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240322/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240322/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -8,17 +8,29 @@ for r1 in range(n):
         for r2 in range(r1,n):
             for c2 in range(c1,m):
                 for r3 in range(r2,n):
-                    for c3 in range(c2,m):
-                        for r4 in range(r3,n):
-                            for c4 in range(c3,n):
-                                temp = 0
-                                if r2 == r3 and c2 == c3: continue
-                                for r in range(r1,r2+1):
-                                    for c in range(c1,c2+1):
-                                        temp += board[r][c]
-                                for r in range(r3,r4+1):
-                                    for c in range(c3,c4+1):
-                                        temp += board[r][c]
-                                ans = max(ans,temp)
+                    if r2 == r3:
+                        for c3 in range(c2+1,m):
+                            for r4 in range(r3,n):
+                                for c4 in range(c3,n):
+                                    temp = 0
+                                    for r in range(r1,r2+1):
+                                        for c in range(c1,c2+1):
+                                            temp += board[r][c]
+                                    for r in range(r3,r4+1):
+                                        for c in range(c3,c4+1):
+                                            temp += board[r][c]
+                                    ans = max(ans,temp)
+                    else:
+                        for c3 in range(m):
+                            for r4 in range(r3,n):
+                                for c4 in range(c3,n):
+                                    temp = 0
+                                    for r in range(r1,r2+1):
+                                        for c in range(c1,c2+1):
+                                            temp += board[r][c]
+                                    for r in range(r3,r4+1):
+                                        for c in range(c3,c4+1):
+                                            temp += board[r][c]
+                                    ans = max(ans,temp)
 
 print(ans)
