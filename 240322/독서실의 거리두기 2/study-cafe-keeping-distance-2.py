e911221dab144c219c14e6dc63857fe60f848c24
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 21:21:48 +0900

    Title: 독서실의 거리두기 2, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240322/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240322/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -11,15 +11,24 @@ for i in range(N):
             idx = (start+i)//2
         start = i
 
-dist1 = N-1-start
-
 library[idx] = '1'
 
-dist = 100000
+dist1 = 100000
 start = 0
 for i in range(1,N):
     if library[i] == '1':
-        dist = min(dist,i-start)
+        dist1 = min(dist1,i-start)
         start = i
 
-print(max(dist,dist1))
+dist2 = 0
+library[idx] = '0'
+if library[-1] == '0':
+    library[-1] = '1'
+    dist2 = 100000
+    start = 0
+    for i in range(1,N):
+        if library[i] == '1':
+            dist2 = min(dist2,i-start)
+            start = i
+
+print(max(dist1,dist2))
