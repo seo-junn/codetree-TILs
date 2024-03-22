fb96c87c2a118b8035ac04b349c8cd11e67aaec2
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 20:03:36 +0900

    Title: 적석과 흑석 2, Time: 1087ms, Memory: 42MB, Status: Passed - CodeTree

--- 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,22 +1,21 @@
 import sys
-from bisect import bisect_left, bisect_right
+from sortedcontainers import SortedSet
+
 
 C, N = map(int,sys.stdin.readline().split())
 reds = [int(sys.stdin.readline()) for _ in range(C)]
 blacks = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-reds.sort()
-cache = [0]*C
-blacks.sort(key=lambda x:(x[1]-x[0],x[0]))
+red = SortedSet(reds)
+blacks.sort(key=lambda x:(x[1],x[0]))
 
 count = 0
-for lb,rb in blacks:
-    l_idx = bisect_left(reds,lb)
-    r_idx = bisect_right(reds,rb)
-    for idx in range(l_idx,r_idx):
-        if cache[idx] == 0 and lb <= reds[idx] <= rb:
-            cache[idx] += 1
+for lb, rb in blacks:
+    idx = red.bisect_left(lb)
+    if idx != len(red):
+        t = red[idx]
+        if t <= rb:
             count += 1
-            break
+            red.remove(t)
 
 print(count)
