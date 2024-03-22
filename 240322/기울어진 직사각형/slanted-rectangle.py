086f3f43ab965b7b1afa806632a77693aad30627
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 16:29:32 +0900

    Title: 기울어진 직사각형, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,64 @@
+import sys
+
+n = int(sys.stdin.readline())
+board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+ans = 0
+for row in range(n):
+    for col in range(n):
+        for odd in range(1,n):
+            for even in range(1,n):
+                temp = 0
+                pr,pc = row,col
+                fail = False
+                for _ in range(odd):
+                    nr,nc = pr-1,pc+1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        temp += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        temp = 0
+                        fali = True
+                        break
+                
+                if fail: continue
+
+                for _ in range(even):
+                    nr,nc = pr-1,pc-1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        temp += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        temp = 0
+                        fali = True
+                        break
+                
+                if fail: continue
+
+                for _ in range(odd):
+                    nr,nc = pr+1,pc-1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        temp += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        temp = 0
+                        fali = True
+                        break
+                
+                if fail: continue
+
+                for _ in range(even):
+                    nr,nc = pr+1,pc+1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        temp += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        temp = 0
+                        fali = True
+                        break
+                
+                if fail: continue
+
+                ans = max(ans,temp)
+
+print(ans)
