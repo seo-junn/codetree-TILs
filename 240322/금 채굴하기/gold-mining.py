6b5ecaefca7e3ede82ff96198d8f3c46dc6abae2
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 15:59:36 +0900

    Title: 금 채굴하기, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240322/금 채굴하기/gold-mining.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+dr = [-1,0,1,0]
+dc = [0,1,0,-1]
+
+n,m = map(int,sys.stdin.readline().split())
+board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        gold = 0
+        q = deque()
+        q.append((i,j))
+        cache = [[0]*n for _ in range(n)]
+        for depth in range(n):
+            nq = deque()
+            while q:
+                pr,pc = q.popleft()
+                if board[pr][pc]: gold += 1
+                for i in range(4):
+                    nr,nc = pr+dr[i],pc+dc[i]
+                    if 0 <= nr < n and 0 <= nc < n and cache[nr][nc] == 0:
+                        cache[nr][nc] += 1
+                        nq.append((nr,nc))
+            cost = depth**2 + (depth+1)**2
+            if cost <= gold*m:
+                ans = max(ans,gold)
+            q = nq
+
+print(ans)
