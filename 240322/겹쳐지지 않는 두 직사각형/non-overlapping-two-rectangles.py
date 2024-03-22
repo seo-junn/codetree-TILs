9cb2736d771386a3470197b8f20dbc5a86044003
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 16:48:54 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240322/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240322/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -11,7 +11,7 @@ for r1 in range(n):
                     for c3 in range(m):
                         for r4 in range(r3,n):
                             for c4 in range(c3,n):
-                                if r1 <= r3 <= r2 or r1 <= r4 <= r2:
+                                if r1 <= r3 <= r2 or r1 <= r4 <= r2 or (c3 <= c1 <= c4 and c3 <= c2 <= c4):
                                     if c1 <= c3 <= c2 or c1 <= c4 <= c2 or (c3 <= c1 <= c4 and c3 <= c2 <= c4): continue
                                     temp = 0
                                     for r in range(r1,r2+1):
