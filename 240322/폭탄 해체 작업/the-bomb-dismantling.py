ccf456c907f001b8149903f538b5b78615b9a418
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 18:40:11 +0900

    Title: 폭탄 해체 작업, Time: 178ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240322/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,14 +1,27 @@
 import sys
 
 N = int(sys.stdin.readline())
-bombs = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
-bombs.sort(key=lambda x:(x[1],-x[0]))
+bombs = []
+max_time = 0
+for _ in range(N):
+    sc, tc = map(int,sys.stdin.readline().split())
+    bombs.append((sc,tc))
+    if tc > max_time:
+        max_time = tc
+
+timeline = [0]+[0]*max_time
+bombs.sort(key=lambda x:(-x[0],-x[1]))
 
 total_score = 0
-time = 0
 for score, tik in bombs:
-    if tik > time:
+    if timeline[tik] == 0:
+        timeline[tik] += 1
         total_score += score
-        time += 1
+    else:
+        for i in range(tik,0,-1):
+            if timeline[i] == 0:
+                timeline[i] += 1
+                total_score += score
+                break
 
 print(total_score)
