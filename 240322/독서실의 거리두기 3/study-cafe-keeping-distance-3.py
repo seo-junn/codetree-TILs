5a512309c1d6819ce413e6856a37f34a2fc549f6
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 20:57:05 +0900

    Title: 독서실의 거리두기 3, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,24 @@
+N = int(input())
+library = list(input().strip())
+
+dist = 0
+idx = -1
+start = 0
+for i in range(1,N):
+    if library[i] == '1':
+        temp = i-start
+        if temp > dist:
+            dist = temp
+            idx = (start+i)//2
+        start = i
+
+library[idx] = '1'
+
+dist = 10000
+start = 0
+for i in range(1,N):
+    if library[i] == '1':
+        temp = i-start
+        dist = min(dist,temp)
+
+print(dist)
