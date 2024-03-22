8d8bf9eabe7be02e5de2de07bd094cc26b458877
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 11:24:06 +0900

    Title: 이상한 진수 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/이상한 진수 2/awkward-digits-2.py
+++ 240322/이상한 진수 2/awkward-digits-2.py
@@ -1,5 +1,14 @@
 num = input().strip()
 
-num = num.replace('0','1',1)
+is_zero = False
+for ch in num:
+    if ch == '0':
+        is_zero = True
+        break
+
+if is_zero:
+    num = num.replace('0','1',1)
+else:
+    num = num[::-1].replace('1','0',1)[::-1]
 
 print(int(num,2))
