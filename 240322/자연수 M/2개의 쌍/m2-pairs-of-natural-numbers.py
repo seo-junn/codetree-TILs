318f4226bede7a4fea99e9f82ba98266bf234c0f
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 19:00:09 +0900

    Title: 자연수 M/2개의 쌍, Time: 244ms, Memory: 32MB, Status: Passed - CodeTree

--- 240322/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240322/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,23 +1,26 @@
 import sys
 
 N = int(sys.stdin.readline())
-nums = []
+nums = {}
 
 for _ in range(N):
     x, y = map(int,sys.stdin.readline().split())
-    for i in range(x):
-        nums.append(y)
+    nums[y] = x
 
-nums.sort()
+index = list(sorted(nums.keys()))
+left = 0
+right = len(index)-1
 
 max_sum = 0
-
-left = 0
-right = len(nums)-1
-
 while left < right:
-    max_sum = max(max_sum,nums[left]+nums[right])
-    left += 1
-    right -= 1
+    if nums[index[left]] and nums[index[right]]:
+        max_sum = max(max_sum,index[left]+index[right])
+        temp = min(nums[index[left]],nums[index[right]])
+        nums[index[left]] -= temp
+        nums[index[right]] -= temp
+    elif nums[index[left]] == 0:
+        left += 1
+    else:
+        right -= 1
 
 print(max_sum)
