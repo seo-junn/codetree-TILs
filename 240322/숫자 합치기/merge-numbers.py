ab6aef8adc1baddbf23a8fd832771f9e2ce3cb7e
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 17:27:06 +0900

    Title: 숫자 합치기, Time: 273ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240322/숫자 합치기/merge-numbers.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+nums = list(map(int,sys.stdin.readline().split()))
+
+cost = 0
+while len(nums) > 1:
+    a = heapq.heappop(nums)
+    b = heapq.heappop(nums)
+    cost += a+b
+    heapq.heappush(nums,a+b)
+
+print(cost)
