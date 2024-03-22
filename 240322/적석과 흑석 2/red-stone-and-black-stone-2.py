04690027b2825104e08feb3da3b6ef1bb5498cdf
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 19:45:06 +0900

    Title: 적석과 흑석 2, Time: 329ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -4,16 +4,16 @@ C, N = map(int,sys.stdin.readline().split())
 reds = [int(sys.stdin.readline()) for _ in range(C)]
 blacks = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-reds.sort()
-blacks.sort()
+reds.sort(reverse = True)
+blacks.sort(reverse = True)
 
 r, b = 0, 0
 count = 0
 while r < C and b < N:
     if reds[r] < blacks[b][0]:
-        r += 1
-    elif reds[r] > blacks[b][1]:
         b += 1
+    elif reds[r] > blacks[b][1]:
+        r += 1
     else:
         count += 1
         r += 1
