ec4eafd4ccc8837c3608c8dbbb3a8d08dda18ba9
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 19:55:02 +0900

    Title: 적석과 흑석 2, Time: 1133ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240322/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,5 +1,5 @@
 import sys
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
 C, N = map(int,sys.stdin.readline().split())
 reds = [int(sys.stdin.readline()) for _ in range(C)]
@@ -12,8 +12,8 @@ blacks.sort(key=lambda x:(x[1]-x[0],x[0]))
 count = 0
 for lb,rb in blacks:
     l_idx = bisect_left(reds,lb)
-    r_idx = bisect_left(reds,rb)
-    for idx in range(l_idx,(r_idx+1 if r_idx < C-1 else r_idx)):
+    r_idx = bisect_right(reds,rb)
+    for idx in range(l_idx,r_idx):
         if cache[idx] == 0 and lb <= reds[idx] <= rb:
             cache[idx] += 1
             count += 1
