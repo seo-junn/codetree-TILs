04295220243b01b80100a38b2222bb64764a6525
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 22:53:18 +0900

    Title: 수열의 순서 바꾸기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240322/수열의 순서 바꾸기/reorder-sequence.py
+++ 240322/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,30 +1,9 @@
 N = int(input())
 nums = list(map(int,input().split()))
 
-def insertion(i,j):
-    temp = nums[i]
-    for idx in range(i,j):
-        nums[idx] = nums[idx+1]
-    nums[j] = temp
-
 count = 0
-while True:
-    if nums[0] == 1:
-        idx = -1
-        for i in range(N-1):
-            if nums[i] > nums[i+1]:
-                idx = i
-                break
-        if idx == -1: break
-        insertion(0,idx)
-        count += 1
-    else:
-        idx = 0
-        for i in range(1,N):
-            if nums[i] == nums[0]-1:
-                idx = i
-                break
-        insertion(0,idx)
-        count += 1
+for i in range(N-1):
+    if nums[i] > nums[i+1]:
+        count += i+1
 
 print(count)
