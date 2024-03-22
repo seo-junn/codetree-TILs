a39fa72e61033337a3cb35dc56021ef20a723c3f
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 20:48:40 +0900

    Title: 연속된 숫자 만들기 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240322/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -2,11 +2,7 @@ a,b,c = sorted(map(int,input().split()))
 
 if b-a == c-b == 1:
     print(0)
-elif b-a == 1:
-    if c-b == 2: print(1)
-    else: print(2)
-elif c-b == 1:
-    if b-a == 2: print(1)
-    else: print(2)
+elif b-a == 2 or c-b == 2:
+    print(1)
 else:
     print(2)
