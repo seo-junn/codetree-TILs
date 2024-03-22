554fe5d45b0fed63e13ea29d169a433ba35ee56e
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 21:29:54 +0900

    Title: 독서실의 거리두기 2, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240322/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -3,6 +3,7 @@ library = list(input().strip())
 
 start = -1
 dist = 0
+idx = -1
 for i in range(N):
     if library[i] == '1':
         if start == -1:
@@ -14,29 +15,31 @@ for i in range(N):
             idx = (start+i)//2
         start = i
 
-library[idx] = '1'
+dist1 = 0
+if idx != -1:
+    library[idx] = '1'
 
-dist1 = 100000
-start = -1
-for i in range(N):
-    if library[i] == '1':
-        if start == -1:
+    dist1 = 100000
+    start = -1
+    for i in range(N):
+        if library[i] == '1':
+            if start == -1:
+                start = i
+                continue
+            dist1 = min(dist1,i-start)
             start = i
-            continue
-        dist1 = min(dist1,i-start)
-        start = i
+    library[idx] = '0'
 
 dist2 = 0
-library[idx] = '0'
 if library[-1] == '0':
     library[-1] = '1'
     dist2 = 100000
     start = -1
     for i in range(N):
-        if start == -1:
-            start = i
-            continue
         if library[i] == '1':
+            if start == -1:
+                start = i
+                continue
             dist2 = min(dist2,i-start)
             start = i
     library[-1] = '0'
@@ -47,10 +50,10 @@ if library[0] == '0':
     dist3 = 100000
     start = -1
     for i in range(N):
-        if start == -1:
-            start = i
-            continue
         if library[i] == '1':
+            if start == -1:
+                start = i
+                continue
             dist3 = min(dist3,i-start)
             start = i
 
