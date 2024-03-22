44fe0efa7d1a9285dfe4f92de39736228d516a73
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 16:02:43 +0900

    Title: 금 채굴하기, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240322/금 채굴하기/gold-mining.py
+++ 240322/금 채굴하기/gold-mining.py
@@ -10,7 +10,7 @@ board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 ans = 0
 for i in range(n):
     for j in range(n):
-        gold = 0
+        gold = board[i][j]
         q = deque()
         q.append((i,j))
         cache = [[0]*n for _ in range(n)]
