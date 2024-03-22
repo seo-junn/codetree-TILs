817294b9354624d7e195d688e677afc9a3aa2296
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-03-22 14:45:34 +0900

    Title: 바구니 안의 사탕 2, Time: 146ms, Memory: 28MB, Status: Passed - CodeTree

--- 240322/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240322/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -7,16 +7,12 @@ for _ in range(N):
     candy,pos = map(int,sys.stdin.readline().split())
     basket[pos] += candy
 
-now = sum(basket[:2*K+2])
-ans = now
-start = 0
-end = 2*K+2
-
-while end < N:
-    now -= basket[start]
-    now += basket[end]
-    ans = max(ans,now)
-    start += 1
-    end += 1
+ans = 0
+for c in range(100):
+    temp = 0
+    for i in range(c-K,c+K+1):
+        if 0 <= i <= 100:
+            temp += basket[i]
+    ans = max(ans,temp)
 
 print(ans)
