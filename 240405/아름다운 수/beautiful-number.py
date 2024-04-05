38ea0eda6de841d98e07bf8e1001cd21f3d7a610
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-05 18:19:43 +0900

    Title: 아름다운 수, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/아름다운 수/beautiful-number.py
+++ 240405/아름다운 수/beautiful-number.py
@@ -1,15 +1,11 @@
 n = int(input())
 
-cache = [-1]*(n+1)
-cache[0] = 1
 def find(n):
-    if cache[n] != -1:
-        return cache[n]
+    if n == 0: return 1
     res = 0
     for i in range(1,5):
         if n >= i:
             res += find(n-i)
-    cache[n] = res
     return res
 
 print(find(n))
