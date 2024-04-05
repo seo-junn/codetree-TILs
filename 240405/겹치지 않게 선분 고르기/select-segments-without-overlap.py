27629859f5bad95cb55e192cbba1b4eb72764916
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-05 20:50:26 +0900

    Title: 겹치지 않게 선분 고르기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240405/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240405/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -2,17 +2,13 @@ n = int(input())
 lines = [tuple(map(int,input().split())) for _ in range(n)]
 lines.sort()
 
-cache_o = [-1]*n
-cache_x = [-1]*n
 def count_case(start,idx):
     if idx >= n:
         return 0
     res = 0
-    if lines[idx][0] > start:
-        if cache_o[idx] == -1: cache_o[idx] = count_case(lines[idx][1],idx+1)+1
-        res = max(res,cache_o[idx])
-    if cache_x[idx] == -1: cache_x[idx] = count_case(start,idx+1)
-    res = max(res,cache_x[idx])
+    if lines[idx][0] > start:    
+        res = max(res,count_case(lines[idx][1],idx+1)+1)
+    res = max(res,count_case(start,idx+1))
     return res
-            
+
 print(count_case(0,0))
