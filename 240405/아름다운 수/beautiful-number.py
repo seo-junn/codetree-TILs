dee19feaf763da221a16581a9336d7e70f60bc25
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-05 18:17:50 +0900

    Title: 아름다운 수, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240405/아름다운 수/beautiful-number.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+def find(n):
+    if n == 0: return 1
+    res = 0
+    for i in range(1,5):
+        if n >= i:
+            res += find(n-i)
+    return res
+
+print(find(n))
