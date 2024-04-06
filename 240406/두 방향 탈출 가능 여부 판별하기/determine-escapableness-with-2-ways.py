4bcfb4d40846a0e18ddd459a5ebe6ee1d81a6ae7
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-06 20:36:20 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 177ms, Memory: 27MB, Status: Passed - CodeTree

--- 240406/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240406/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,7 +6,6 @@ board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 dr = [0, 1]
 dc = [1, 0]
 
-escape = False
 cache = [[0]*m for _ in range(n)]
 
 def search(pr,pc):
@@ -14,12 +13,8 @@ def search(pr,pc):
         nr, nc = pr+dr[i], pc+dc[i]
         if 0 <= nr < n and 0 <= nc < m and board[nr][nc] and cache[nr][nc] == 0:
             cache[nr][nc] += 1
-            if nr == n-1 and nc == m-1:
-                global escape
-                escape = True
-                break
             search(nr,nc)
 
 cache[0][0] += 1
 search(0,0)
-print(1 if escape else 0)
+print(cache[-1][-1])
