6ff387b93606c0313c74dabe52b3da6a98a61aec
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-06 20:27:01 +0900

    Title: 그래프 탐색, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240406/그래프 탐색/graph-traversal.py
+++ 240406/그래프 탐색/graph-traversal.py
@@ -1,22 +1,25 @@
 import sys
 
 N, M = map(int,sys.stdin.readline().split())
-graph = [[0]*N for _ in range(N)]
+graph = [[] for _ in range(N)]
 
 for _ in range(M):
     s,e = map(int,sys.stdin.readline().split())
-    graph[s-1][e-1] += 1
-    graph[e-1][s-1] += 1
+    graph[s-1].append(e-1)
+    graph[e-1].append(s-1)
 
 visited = [0]*N
+count = 0
 
 def search(dot):
-    for i in range(N):
-        if graph[dot][i] and visited[i] == 0:
-            visited[i] += 1
-            search(i)
+    global count
+    for next_dot in graph[dot]:
+        if visited[next_dot] == 0:
+            visited[next_dot] += 1
+            count += 1
+            search(next_dot)
 
 visited[0] = 1
 search(0)
 
-print(sum(visited)-1)
+print(count)
