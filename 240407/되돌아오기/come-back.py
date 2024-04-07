cacd4a22fdfd317d44af29eb57f03b41618e812c
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-07 11:38:32 +0900

    Title: 되돌아오기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/되돌아오기/come-back.py
@@ -0,0 +1,18 @@
+dx = {'N':0,'S':0,'E':1,'W':-1}
+dy = {'N':1,'S':-1,'E':0,'W':0}
+
+x, y = 0,0
+time = 0
+reached = False
+
+for _ in range(int(input())):
+    d, dist = input().split()
+    for i in range(int(dist)):
+        time += 1
+        x,y = x+dx[d],y+dy[d]
+        if x == 0 and y == 0:
+            reached = True
+            break
+    if reached: break
+
+print(time)
