ad4e934bcb7312261086e493b7a96126d2c79f3b
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-08 20:32:53 +0900

    Title: 기울어진 직사각형, Time: 245ms, Memory: 27MB, Status: Passed - CodeTree

--- 240408/기울어진 직사각형/slanted-rectangle.py
+++ 240408/기울어진 직사각형/slanted-rectangle.py
@@ -3,35 +3,53 @@ import sys
 n = int(sys.stdin.readline())
 board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-def search(row,col):
-    max_val = 0
-    for depth in range(2,row+1):
-        for ur in range(1,min(depth,n-col)):
-            ul = depth-ur
-            val = 0
-            pr,pc = row,col
-            for i in range(ur):
-                nr,nc = pr-1,pc+1
-                pr,pc = nr,nc
-                val += board[pr][pc]
-            for i in range(ul):
-                nr,nc = pr-1,pc-1
-                pr,pc = nr,nc
-                val += board[pr][pc]
-            for i in range(ur):
-                nr,nc = pr+1,pc-1
-                pr,pc = nr,nc
-                val += board[pr][pc]
-            for i in range(ul):
-                nr,nc = pr+1,pc+1
-                pr,pc = nr,nc
-                val += board[pr][pc]
-            max_val = max(max_val,val)
-    return max_val
-
 ans = 0
 for row in range(2,n):
     for col in range(1,n-1):
-        ans = max(ans,search(row,col))
+        for odd in range(1,n):
+            for even in range(1,n):
+                val = 0
+                pr,pc = row,col
+                fail = False
+                for i in range(odd):
+                    nr,nc = pr-1,pc+1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        val += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        fail = True
+                        break
+                if fail: continue
 
+                for i in range(even):
+                    nr,nc = pr-1,pc-1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        val += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        fail = True
+                        break
+                if fail: continue
+
+                for i in range(odd):
+                    nr,nc = pr+1,pc-1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        val += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        fail = True
+                        break
+                if fail: continue
+
+                for i in range(even):
+                    nr,nc = pr+1,pc+1
+                    if 0 <= nr < n and 0 <= nc < n:
+                        val += board[nr][nc]
+                        pr,pc = nr,nc
+                    else:
+                        fail = True
+                        break
+                if fail: continue
+                
+                ans = max(ans,val)
 print(ans)
