9729ab1702f7c598ed501dfe484ca09f180d4e30
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 18:23:06 +0900

    Title: 정원 입장은 선착순, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240410/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -2,32 +2,27 @@ import sys
 import heapq
 
 N = int(sys.stdin.readline())
-waiting = []
+waiting = 0
 people = []
 wait_line = []
 
 for i in range(1, N + 1):
     a, t = map(int, sys.stdin.readline().split())
-    heapq.heappush(people, (a, i, t))
+    people.append((a,i,t))
+
+people.sort(key=lambda x:x[0])
 
 time = 0
-while people or wait_line:
-    if wait_line:
-        while people and people[0][0] < time:
-            a, i, t = heapq.heappop(people)
-            heapq.heappush(wait_line, (i, a, t))
-        num, arrive, delay = heapq.heappop(wait_line)
-        wait = time - arrive
-        heapq.heappush(waiting, -wait)
-        time += delay
-    else:
-        if people[0][0] >= time:
-            arrive, num, delay = heapq.heappop(people)
-            time = arrive + delay
-            heapq.heappush(waiting,0)
-        else:
-            while people and people[0][0] < time:
-                a,i,t = heapq.heappop(people)
-                heapq.heappush(wait_line,(i,a,t))
+for arrive, num, delay in people:
 
-print(-waiting[0])
+    while arrive > time and wait_line:
+        n_num,n_arrive,n_delay = heapq.heappop(wait_line)
+        waiting = max(waiting,time-n_arrive)
+        time += n_delay
+
+    if arrive > time:
+        time = arrive + delay
+    else:
+        heapq.heappush(wait_line,(num,arrive,delay))
+
+print(waiting)
