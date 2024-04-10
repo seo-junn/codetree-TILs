2a5f61f4f2a8c2444432d4a8a20a04f71269b0f7
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 18:16:58 +0900

    Title: 정원 입장은 선착순, Time: 943ms, Memory: 42MB, Status: Passed - CodeTree

--- 240410/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240410/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -4,28 +4,30 @@ import heapq
 N = int(sys.stdin.readline())
 waiting = []
 people = []
+wait_line = []
 
 for i in range(1, N + 1):
     a, t = map(int, sys.stdin.readline().split())
     heapq.heappush(people, (a, i, t))
 
 time = 0
-while people:
-    if people[0][0] >= time:
-        arrive, num, delay = heapq.heappop(people)
-        time = arrive + delay
-        heapq.heappush(waiting, 0)
-    else:
-        candidate = []
+while people or wait_line:
+    if wait_line:
         while people and people[0][0] < time:
             a, i, t = heapq.heappop(people)
-            heapq.heappush(candidate, (i, a, t))
-        num, arrive, delay = heapq.heappop(candidate)
+            heapq.heappush(wait_line, (i, a, t))
+        num, arrive, delay = heapq.heappop(wait_line)
         wait = time - arrive
         heapq.heappush(waiting, -wait)
         time += delay
-        while candidate:
-            i, a, t = heapq.heappop(candidate)
-            heapq.heappush(people, (a, i, t))
+    else:
+        if people[0][0] >= time:
+            arrive, num, delay = heapq.heappop(people)
+            time = arrive + delay
+            heapq.heappush(waiting,0)
+        else:
+            while people and people[0][0] < time:
+                a,i,t = heapq.heappop(people)
+                heapq.heappush(wait_line,(i,a,t))
 
 print(-waiting[0])
