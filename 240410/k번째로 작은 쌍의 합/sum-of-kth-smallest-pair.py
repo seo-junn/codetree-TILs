47c1aad193816b25a567a5017c8e08ee5b174857
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 16:33:40 +0900

    Title: k번째로 작은 쌍의 합, Time: 167ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240410/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240410/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -5,7 +5,7 @@ n,m,k = map(int,sys.stdin.readline().split())
 arr1 = list(sorted(map(int,sys.stdin.readline().split())))
 arr2 = list(sorted(map(int,sys.stdin.readline().split())))
 
-l,r,comb_num = 1,1,1
+l,r,comb_num = 0,0,0
 
 while l < n and r < m and l*r < k:
     if arr1[l] <= arr2[r]: l += 1
