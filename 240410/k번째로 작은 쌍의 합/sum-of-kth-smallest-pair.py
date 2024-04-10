cf4e17f7b3c79b6542bdfd4a65305160e5d0c534
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 16:56:55 +0900

    Title: k번째로 작은 쌍의 합, Time: 244ms, Memory: 34MB, Status: Wrong Answer - CodeTree

--- 240410/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240410/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -5,7 +5,7 @@ n,m,k = map(int,sys.stdin.readline().split())
 arr1 = list(sorted(map(int,sys.stdin.readline().split())))
 arr2 = list(sorted(map(int,sys.stdin.readline().split())))
 
-l,r,comb_num = 0,0,0
+l,r = 0,0
 
 while l < n and r < m and l*r < k:
     if arr1[l] <= arr2[r]: l += 1
@@ -17,9 +17,12 @@ if r == m and l*r < k:
     while l < n and l*r < k: l += 1
 
 base = []
+bias = min(l,r)
+l,r = min(n,l+bias),min(m,r+bias)
 for i in range(l):
     for j in range(r):
         heapq.heappush(base,-(arr1[i]+arr2[j]))
 
 for _ in range(l*r-k): heapq.heappop(base)
+
 print(-base[0])
