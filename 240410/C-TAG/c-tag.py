6cd1539826349925fd8eda9bb231db0527f0c89b
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 10:57:07 +0900

    Title: C-TAG, Time: 854ms, Memory: 26MB, Status: Passed - CodeTree

--- 240410/C-TAG/c-tag.py
+++ 240410/C-TAG/c-tag.py
@@ -6,10 +6,18 @@ A = [sys.stdin.readline().strip() for _ in range(N)]
 B = [sys.stdin.readline().strip() for _ in range(N)]
 
 count = 0
+base = set()
 for item in combinations(range(M),3):
     idx1,idx2,idx3 = item
-    A_set = set(map(lambda x:(x[idx1],x[idx2],x[idx3]),A))
-    B_set = set(map(lambda x:(x[idx1],x[idx2],x[idx3]),B))
-    if A_set.isdisjoint(B_set): count += 1
+    base.clear()
+    for i in range(N):
+        base.add(A[i][idx1]+A[i][idx2]+A[i][idx3])
+
+    possible = True
+    for i in range(N):
+        if B[i][idx1] + B[i][idx2] + B[i][idx3] in base:
+            possible = False
+            break
+    if possible: count += 1
 
 print(count)
