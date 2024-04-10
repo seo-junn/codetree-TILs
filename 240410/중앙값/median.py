1aed98b65ec59b12ba38317aff416132d780384f
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 15:00:11 +0900

    Title: 중앙값, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/중앙값/median.py
+++ 240410/중앙값/median.py
@@ -2,12 +2,23 @@ import sys
 import heapq
 
 for _ in range(int(sys.stdin.readline())):
+    left_pq = []
+    right_pq = []
     m = int(sys.stdin.readline())
-    arr = []
-    ans = []
     nums = list(map(int,sys.stdin.readline().split()))
-    for i in range(m):
-        heapq.heappush(arr,nums[i])
-        if i%2 == 0:
-            ans.append(heapq.nsmallest((i//2)+1,arr)[-1])
+    mid = nums[0]
+    ans = [mid]
+    for i in range(1,m):
+        if nums[i] < mid:
+            heapq.heappush(left_pq,-nums[i])
+        else:
+            heapq.heappush(right_pq,nums[i])
+        if i%2:
+            if len(left_pq) > len(right_pq):
+                heapq.heappush(right_pq,mid)
+                mid = -heapq.heappop(left_pq)
+            elif len(left_pq) < len(right_pq):
+                heapq.heappush(left_pq,-mid)
+                mid = heapq.heappop(right_pq)
+            ans.append(mid)
     print(*ans)
