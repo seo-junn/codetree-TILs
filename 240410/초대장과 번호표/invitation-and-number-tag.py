f5ba7d057a5ec765d0cc762147e580665bf68f35
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-10 11:32:13 +0900

    Title: 초대장과 번호표, Time: 208ms, Memory: 29MB, Status: Passed - CodeTree

--- 240410/초대장과 번호표/invitation-and-number-tag.py
+++ 240410/초대장과 번호표/invitation-and-number-tag.py
@@ -10,15 +10,26 @@ for _ in range(G):
 
 base.sort(key=lambda x:x[0])
 
-for length, group in base:
-    if length > len(invited)+1: break
-    count = 0
-    invite = 0
-    for p in group:
-        if p not in invited:
-            count += 1
-            invite = p
-    if count == 1:
-        invited.add(invite)
+while True:
+    n_base = []
+    adding = False
+    for length, group in base:
+        if length > len(invited) + 1:
+            n_base.append((length,group))
+            continue
+        count = 0
+        invite = 0
+        for p in group:
+            if p not in invited:
+                count += 1
+                invite = p
+            if count > 1: break
+        if count == 1:
+            invited.add(invite)
+            adding = True
+        elif count > 1:
+            n_base.append((length,group))
+    if not adding: break
+    base = n_base
 
 print(len(invited))
