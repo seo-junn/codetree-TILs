f794bfdcd646cfd2213d40748e94555d3117c230
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-11 11:40:58 +0900

    Title: 괄호 쌍 만들어주기, Time: 113ms, Memory: 26MB, Status: Passed - CodeTree

--- 240411/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240411/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,10 +1,18 @@
 line = input()
 
-count = 0
-for i in range(len(line)-1):
-    if line[i] == '(' and line[i+1] == '(':
-        for j in range(i+2,len(line)-1):
-            if line[j] == ')' and line[j+1] == ')':
-                count +=1
+opened = [0]*len(line)
+closed = [0]*len(line)
 
-print(count)
+for i in range(1,len(line)):
+    if line[i] == '(' and line[i-1] == '(': opened[i] = 1
+
+count = 0
+for i in range(len(line)-2,-1,-1):
+    if line[i] == ')' and line[i+1] == ')': count += 1
+    closed[i] = count
+
+total_count = 0
+for i in range(len(line)-1):
+    total_count += opened[i]*closed[i+1]
+
+print(total_count)
