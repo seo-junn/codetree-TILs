116f31b0ad10b0a090fa93a7390fae01ec27957c
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-11 13:35:54 +0900

    Title: 최소 에너지 비용, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240411/최소 에너지 비용/minimum-energy-cost.py
+++ 240411/최소 에너지 비용/minimum-energy-cost.py
@@ -4,11 +4,15 @@ n = int(sys.stdin.readline())
 energy_consumption = list(map(int,sys.stdin.readline().split()))
 charge_costs = list(map(int,sys.stdin.readline().split()))
 
-min_cost = [0]*(n-1)
-acc_energy = energy_consumption[-1]
-min_cost[n-2] = energy_consumption[-1]*charge_costs[n-2]
-for i in range(n-3,-1,-1):
-    acc_energy += energy_consumption[i]
-    min_cost[i] = min(acc_energy*charge_costs[i],min_cost[i+1]+energy_consumption[i]*charge_costs[i])
+min_val = sys.maxsize
+min_cost = []
+for i in range(n):
+    if charge_costs[i] < min_val:
+        min_val = charge_costs[i]
+    min_cost.append(min_val)
 
-print(min_cost[0])
+total_cost = 0
+for i in range(n-1):
+    total_cost += min_cost[i]*energy_consumption[i]
+
+print(total_cost)
