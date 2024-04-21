3cefb396031a1af1a9cf01a8365d662ed9781091
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-21 20:28:52 +0900

    Title: 코로나 메뉴얼, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/코로나 메뉴얼/covid-manual.py
+++ 240421/코로나 메뉴얼/covid-manual.py
@@ -1,7 +1,8 @@
-f,c = input().split()
-c = int(c)
-
 count = 0
-if f == 'Y' and c >= 37: count += 1
+
+for _ in range(3):
+    f,c = input().split()
+    c = int(c)
+    if f == 'Y' and c >= 37: count += 1
 
 print('E' if count >= 2 else 'N')
