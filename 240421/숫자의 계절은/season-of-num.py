a32371ee7a0d7c803b7c9642a02c7fd40a42add7
seo-junn <67994080+seo-junn@users.noreply.github.com>
2024-04-21 20:16:37 +0900

    Title: 숫자의 계절은, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240421/숫자의 계절은/season-of-num.py
@@ -0,0 +1,16 @@
+base = {
+    3:'Spring',
+    4:'Spring',
+    5:'Spring',
+    6:'Summer',
+    7:'Summer',
+    8:'Summer',
+    9:'Fall',
+    10:'Fall',
+    11:'Fall',
+    12:'Winter',
+    1:'Winter',
+    2:'Winter'
+}
+
+print(base[int(input())])
